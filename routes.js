64311aadb6d4982974de18bec6a3f62c6049c952
Juan Martin Runge <jmrunge@gmail.com>
2014-09-29 14:45:40 -0300

    Added server routes
    Signed-off-by: Juan Martin Runge <jmrunge@gmail.com>

---
+++ routes.js
@@ -0,0 +1,22 @@
+var home     = require('./controllers/home')
+,   services = require('./controllers/services')
+,   events   = require('./controllers/events')
+;
+
+module.exports.initialize = function(app) {
+    app.get('/', home.index);
+    app.get('/server', home.get);
+    app.get('/server/:id', home.get);
+//    app.post('/server', home.updateEit);
+    app.put('/server/:id', home.updateEit);
+    app.get('/api/services', services.index);
+    app.get('/api/services/:id', services.getById);
+    app.post('/api/services', services.add);
+    app.put('/api/services', services.update);
+    app.delete('/api/services/:id', services.delete);
+    app.get('/api/events', events.index);
+    app.get('/api/events/:id', events.getById);
+    app.post('/api/events', events.add);
+    app.put('/api/events', events.update);
+    app.delete('/api/events/:id', events.delete);
+};
