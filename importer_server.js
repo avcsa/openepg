a74a61ad0244c2306b1e8c4f6dea12aa841b9aae
Juan Martin Runge <jmrunge@gmail.com>
2014-10-30 18:25:39 -0300

    Modified importer to use epg lib massive functions
    Signed-off-by: Juan Martin Runge <jmrunge@gmail.com>

--- importer_server.js
+++ importer_server.js
@@ -13,75 +13,17 @@ importer.on("error", function(err) {
 });
 importer.on("services", function(services) {
     console.log("Received services from importer");
-    _.each(services, function(service) {
-        try {
-            var serv = epg.listServices();
-            if (serv)
-                serv = _.find(serv, function(s) {return s.serviceId === parseInt(service.serviceId);});
-            
-            if (!serv) {
-                console.log("Creating service", service);
-                serv = {};
-                serv.serviceId = service.serviceId;
-                if (service.originalNetworkId)
-                    serv.originalNetworkId = service.originalNetworkId;
-                else
-                    serv.originalNetworkId = service.serviceId;
-                if (service.transportStreamId)
-                    serv.transportStreamId = service.transportStreamId;
-                else
-                    serv.transportStreamId = service.serviceId;
-                if (service.comment)
-                    serv.comment = service.comment;
-                else
-                    serv.comment = 'Service ' + service.serviceId;
-                if (service.signalId)
-                    serv.signalId = service.signalId;
-                else
-                    serv.signalId = service.serviceId;
-            } else {
-                console.log("Updating service", service);
-                if (service.originalNetworkId)
-                    serv.originalNetworkId = service.originalNetworkId;
-                if (service.transportStreamId)
-                    serv.transportStreamId = service.transportStreamId;
-                if (service.comment)
-                    serv.comment = service.comment;
-                if (service.signalId)
-                    serv.signalId = service.signalId;
-            }
-            epg.addService(serv);
-            console.log("Service added/updated");
-            var cant = epg.deleteEvents({serviceId: serv.serviceId});
-            console.log(cant + " events removed from service", serv.serviceId);
-        } catch (err) {
-            console.error("Error adding/updating service", err, service);
-        }
-    });
+    epg.addServices(services);
 });
 importer.on("events", function(events) {
     console.log("Received events from importer");
-    var updateEit = _.after(events.length, function() {
-        console.log("Updating EIT after import");
-        var ret = epg.updateEit();
-        if (ret) {
-            console.log("EIT updated, updating Carousel");
-            epg.updateCarousel();
-        }    
-    });
-    var eventId = epg.getMaxEventId();
-    _.each(events, function(event) {
-        eventId++;
-        event.id = eventId;
-        console.log("Adding event", event.title);
-        try {
-            var evt = epg.addEvent(event);
-            console.log("Event added");
-            updateEit();
-        } catch (err) {
-            console.error("Error adding event", err, event);
-        }
-    });
+    epg.addEvents(events);
+    console.log("Updating EIT after import");
+    var ret = epg.updateEit();
+    if (ret) {
+        console.log("EIT updated, updating Carousel");
+        epg.updateCarousel();
+    }        
 });
 importer.on("run", function() {
     console.log("Starting import");
